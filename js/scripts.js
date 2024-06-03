9b48f83843eb6d2567e4ecc02687a941b52ea0e1
aeueoe <drom.jul@gmail.com>
2024-06-03 09:56:42 +0200

    added an array of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert("Hello World");
-
-let favoriteFood = "Calzone";
-
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "paras", type: "bug, grass", speed: "25" },
+  { name: "vileplume", type: "grass, poison", speed: "50" },
+  { name: "poliwag", type: "water", speed: "90" },
+];
